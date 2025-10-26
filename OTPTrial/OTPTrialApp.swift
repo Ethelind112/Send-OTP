f4019d1c7cb31ea0d8330d9beec4d34fcca2430a
Ethelind <Ethelindsm@gmail.com>
2025-10-26 11:45:51 +0700

    Initial Commit

---
+++ OTPTrial/OTPTrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTPTrialApp.swift
+//  OTPTrial
+//
+//  Created by Ethelind Septiani Metta on 26/10/25.
+//
+
+import SwiftUI
+
+@main
+struct OTPTrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
